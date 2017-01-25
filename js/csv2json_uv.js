3ec200b45bf19bda0eccf0a887df394be7786154
unknown <syuvashree1995@gmail.com>
2017-01-25 15:58:47 +0530

    completed black and white box testing

--- js/csv2json_uv.js
+++ js/csv2json_uv.js
@@ -1,6 +1,13 @@
+module.exports = (function(year) {
+    if(!year || typeof year !== 'number')
+    {
+        throw Error("Not a number");
+    }
+
 const fs = require('fs');
 
 let rowdata;
+let title = [];
 let myData = [];
 let myData1 = [];
 let linenum = 0;
@@ -9,13 +16,12 @@ let arrest = 0;
 let type = 0;
 let desc = 0;
 let readline = require('readline');
-
-for(let i=0;i<16;i++)
+for(let i = 0; i < 16; i = i + 1)
 {
-  let assaultobj = {"Year":0,"ARRESTED":0,"NOT ARRESTED":0}
-  let theftobj = {"Year":0,"OVER $500":0,"$500 AND UNDER": 0}
-  theftobj['Year']=2001+i;
-  assaultobj['Year']=2001+i;
+  let assaultobj = {'Year':0,"ARRESTED":0,"NOT_ARRESTED":0}
+  let theftobj = {"Year":0,"OVER_$500":0,"UNDER_$500": 0}
+  theftobj.Year=2001+i;
+  assaultobj.Year=2001+i;
   myData[i]=theftobj;
   myData1[i]=assaultobj;
 }
@@ -23,41 +29,39 @@ for(let i=0;i<16;i++)
 
 let rd = readline.createInterface({
     input: fs.createReadStream('../inputdata/crimedata.csv'),
-    output: process.stdout,
+    // output: process.stdout,
     terminal: false
 });
 
 rd.on('line', function(line) {
-let row = line;
-
-  if(linenum==0)
+  let row = line;
+  if(linenum === 0)
   {
     title = row.trim().split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
     index = title.indexOf('Year');
     arrest = title.indexOf('Arrest');
     type = title.indexOf('Primary Type');
     desc = title.indexOf('Description');
-    linenum++;
+    linenum = linenum + 1;
   }
-let exists=0;
-let exists1=0;
-
+  else {
   rowdata = row.trim().split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
   if(rowdata[type] === "THEFT" && rowdata[desc] === "$500 AND UNDER")
   {
-    myData[rowdata[index]-2001]["$500 AND UNDER"]++
+    myData[rowdata[index]-2001].UNDER_$500 = myData[rowdata[index]-2001].UNDER_$500 + 1;
   }
   else if(rowdata[type] === "THEFT" && rowdata[desc] === "OVER $500")
   {
-    myData[rowdata[index]-2001]["OVER $500"]++
+    myData[rowdata[index]-2001].OVER_$500 = myData[rowdata[index]-2001].OVER_$500 + 1;
   }
   else if(rowdata[type] === "ASSAULT" && rowdata[arrest] === "true")
   {
-    myData1[rowdata[index]-2001]["ARRESTED"]++
+    myData1[rowdata[index]-2001].ARRESTED = myData1[rowdata[index]-2001].ARRESTED + 1;
   }
   else if(rowdata[type] === "ASSAULT" && rowdata[arrest] === "false")
   {
-    myData1[rowdata[index]-2001]["NOT ARRESTED"]++
+    myData1[rowdata[index]-2001].NOT_ARRESTED = myData1[rowdata[index]-2001].NOT_ARRESTED + 1;
+  }
   }
 });
 
@@ -66,3 +70,5 @@ rd.on('close',function()
 fs.writeFileSync('../outputdata/theft.json',JSON.stringify(myData));
 fs.writeFileSync('../outputdata/assault.json',JSON.stringify(myData1));
 });
+return "JSON written successfully";
+}(2001));
