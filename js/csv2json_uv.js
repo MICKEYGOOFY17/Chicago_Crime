2e7687cc52087d600363379b0728221f0d388159
vagrant <syuvashree1995@gmail.com>
2017-01-30 04:18:55 +0000

    refactored code

--- js/csv2json_uv.js
+++ js/csv2json_uv.js
@@ -1,7 +1,8 @@
-module.exports = (function(year) {
+
+var a = (year) => {
     if(!year || typeof year !== 'number')
     {
-        throw Error("Not a number");
+        throw Error('Not a number');
     }
 
 const fs = require('fs');
@@ -16,23 +17,28 @@ let arrest = 0;
 let type = 0;
 let desc = 0;
 let readline = require('readline');
+
+/* creating object for 16 years from 2001 to 2016 */
 for(let i = 0; i < 16; i = i + 1)
 {
-  let assaultobj = {'Year':0,"ARRESTED":0,"NOT_ARRESTED":0}
-  let theftobj = {"Year":0,"OVER_$500":0,"UNDER_$500": 0}
-  theftobj.Year=2001+i;
-  assaultobj.Year=2001+i;
-  myData[i]=theftobj;
-  myData1[i]=assaultobj;
+  let assaultobj = {Year: 0, ARRESTED: 0, NOT_ARRESTED: 0};
+  let theftobj = {Year: 0, OVER_$500: 0, UNDER_$500: 0};
+  theftobj.Year = 2001 + i;
+  assaultobj.Year = 2001 + i;
+  myData[i] = theftobj;
+  myData1[i] = assaultobj;
 }
+/* objects are created and pushed into my data and mydata1 array */
 
 
+/* event emitter is created */
 let rd = readline.createInterface({
     input: fs.createReadStream('../inputdata/crimedata.csv'),
-    // output: process.stdout,
+    output: process.stdout ,
     terminal: false
 });
 
+/* event for each line */
 rd.on('line', function(line) {
   let row = line;
   if(linenum === 0)
@@ -45,30 +51,39 @@ rd.on('line', function(line) {
     linenum = linenum + 1;
   }
   else {
+    /* Splits each line based on , */
   rowdata = row.trim().split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-  if(rowdata[type] === "THEFT" && rowdata[desc] === "$500 AND UNDER")
+  /* increments the array value based on the theft and assault cases */
+  if(rowdata[type] === 'THEFT' && rowdata[desc] === '$500 AND UNDER')
   {
-    myData[rowdata[index]-2001].UNDER_$500 = myData[rowdata[index]-2001].UNDER_$500 + 1;
+    myData[rowdata[index] - 2001].UNDER_$500 = myData[rowdata[index] - 2001].UNDER_$500 + 1;
   }
-  else if(rowdata[type] === "THEFT" && rowdata[desc] === "OVER $500")
+  else if(rowdata[type] === 'THEFT' && rowdata[desc] === 'OVER $500')
   {
-    myData[rowdata[index]-2001].OVER_$500 = myData[rowdata[index]-2001].OVER_$500 + 1;
+    myData[rowdata[index] - 2001].OVER_$500 = myData[rowdata[index] - 2001].OVER_$500 + 1;
   }
-  else if(rowdata[type] === "ASSAULT" && rowdata[arrest] === "true")
+  else if(rowdata[type] === 'ASSAULT' && rowdata[arrest] === 'true')
   {
-    myData1[rowdata[index]-2001].ARRESTED = myData1[rowdata[index]-2001].ARRESTED + 1;
+    myData1[rowdata[index] - 2001].ARRESTED = myData1[rowdata[index] - 2001].ARRESTED + 1;
   }
-  else if(rowdata[type] === "ASSAULT" && rowdata[arrest] === "false")
+  else if(rowdata[type] === 'ASSAULT' && rowdata[arrest] === 'false')
   {
-    myData1[rowdata[index]-2001].NOT_ARRESTED = myData1[rowdata[index]-2001].NOT_ARRESTED + 1;
+    myData1[rowdata[index] - 2001].NOT_ARRESTED = myData1[rowdata[index] - 2001].NOT_ARRESTED + 1;
   }
   }
 });
 
-rd.on('close',function()
+/* function after the file is read */
+rd.on('close', function()
 {
-fs.writeFileSync('../outputdata/theft.json',JSON.stringify(myData));
-fs.writeFileSync('../outputdata/assault.json',JSON.stringify(myData1));
+fs.writeFileSync('../outputdata/theft.json', JSON.stringify(myData));
+fs.writeFileSync('../outputdata/assault.json', JSON.stringify(myData1));
 });
-return "JSON written successfully";
-}(2001));
+return 'JSON written successfully';
+};
+
+a(2001);
+
+module.exports = {
+  a
+};
